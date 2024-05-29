be492f642bdd669aa93ee1178f803af40c363ce8
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-29 14:56:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import MainApp from './components/MainApp.vue'
+import axios from "axios"
+
+// ******************AXIOS TESTATO E FUNZIONANTE
+// mounted: {
+//   axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0').then((r)=> {
+//     console.log(r);
+//   })
+// }
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
