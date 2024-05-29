54eccf4ccc05b0b58a2f6c731e37a479450e6f9b
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-29 15:39:51 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@ import MainApp from './components/MainApp.vue'
 import axios from "axios"
 
 // ******************AXIOS TESTATO E FUNZIONANTE
-// mounted: {
-//   axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0').then((r)=> {
-//     console.log(r);
-//   })
-// }
+mounted: {
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDdiZWJhNTQ1NDZkOTg0YzRlYjJmZTY5NDM2YjFiYyIsInN1YiI6IjY2NTcyNmQ4ODBiNTM3ZjA4OTcwOTgyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L0pc_jyclpXbulnDXm6e5m6NxVO4EpiSNAveWbVJJ14'
+  }
+  axios.get('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1').then((r)=> {
+    console.log(r);
+  })
+}
 </script>
 
 <template>
