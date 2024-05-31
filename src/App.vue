5091f21787ef0ad2354d53520674affdb051a40f
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-31 13:36:46 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,26 @@ export default {
     
   },
   mounted() {
-  }
+    const options = {
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/discover/movie?api_key=a07beba54546d984c4eb2fe69436b1bc',
+                params: {sort_by: 'popularity.desc', include_video: 'false', include_adult: 'false', language: 'it', page: '1' },
+                headers: {
+                    accept: 'application/json',
+                }
+            };
+
+            axios
+                .request(options)
+                .then((response) => {
+                    this.store.movies = response.data.results
+                    console.log(this.store.movies);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+    
+  },
 }
 </script>
 
